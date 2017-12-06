40fcfec92d3fc1560f35862845ad3b89ea959e5e
Christophe Bialobos <christophe@bialobos.net>
2017-12-06 23:03:34 +0100

    soirée maison

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const favicon = require('serve-favicon');
-const url = require('url');
-const querystring = require('querystring');
+const bodyParser = require('body-parser');
+var cookiesession = require('cookie-session')
 
 const app = express();
-let totodolist = [];
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.cookieParser());
+app.use(express.cookieSession());
+app.use(app.router);
 
 app.use(express.static(`${__dirname}/public`))
   .use(favicon(`${__dirname}/public/favicon.ico`));
 
 app.get('/', (req, res) => {
-  res.render('list.ejs', { totolist: totodolist });
+  res.render('list.ejs', { todolist: req.session.todolist || [] });
 });
 
-app.post('/todolist', (req, res) => {
-  const params = querystring.parse(url.parse(req.url).query);
-  if ('append' in params) {
-    totodolist.push(params.append);
+app.post('/todo/add', (req, res) => {
+  if (req.body.newitem !== undefined) {
+    if (req.session.todolist === undefined) {
+      req.session.todolist = [];
+    }
+
+    req.session.tododolist.push(req.body.newitem);
+    res.render('list.ejs', { todolist: req.session.todolist });
   }
 });
 
+app.post('/todo/delete/', (req, res) => { 
+  const newLocal = req.session.tododolist;
+
+  let pos = newLocal.indexOf(req.body.id);
+  newLocal.splice(pos, 1);
+  res.render('list.ejs', { todolist: newLocal });
+});
+
 app.listen(8080);
+
